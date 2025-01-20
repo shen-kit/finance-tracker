625b7a9a3a95858dc1eef768770de2edf140a057
Shen-Kit Hia <shenkit108+1@gmail.com>
2025-01-20 13:06:05 +1100

    adds required function headers with docstrings and type hints

--- finance_tracker.py
+++ finance_tracker.py
@@ -3,8 +3,11 @@ import sqlite3
 
 import yfinance as yf
 
+"""
+Helper Functions
+"""
 
-def get_stock_price(stock_code) -> float:
+def get_stock_price(stock_code: str) -> float:
     """
     Uses yfinance (yahoo finance backend) to get the most recent stock price.
     Calculates stock price as the average of the bid and ask prices.
@@ -15,10 +18,13 @@ def get_stock_price(stock_code) -> float:
     info = yf.Ticker(stock_code).info
     return (info["bid"] + info["ask"]) / 2
 
+"""
+Database Initialisation
+"""
 
-def initialise_db(base_dir):
+def initialise_db(base_dir: str) -> tuple[sqlite3.Connection, sqlite3.Cursor]:
 
-    def create_tables(cur):
+    def create_tables(cur: sqlite3.Cursor):
         cur.execute(
             """CREATE TABLE IF NOT EXISTS CATEGORY (
             cat_id INT PRIMARY KEY,
@@ -42,7 +48,7 @@ def initialise_db(base_dir):
             inv_qty    SMALLINT   NOT NULL);"""
         )
 
-    def create_default_category(cur):
+    def create_default_category(cur: sqlite3.Cursor):
         """create default fallback category"""
         if cur.execute("SELECT * FROM CATEGORY;").fetchone() is None:
             cur.execute("INSERT INTO CATEGORY (cat_id, cat_name) VALUES (1, '-/-')")
@@ -54,10 +60,95 @@ def initialise_db(base_dir):
     create_default_category(cur)
     conn.commit()
 
-    return conn, cur
+    return (conn, cur)
 
+
+"""
+Querying the Database
+"""
+
+def display_month_report() -> None:
+    """
+    Display a detailed report on the month:
+        - summary: income, expenditure, net change
+        - records in tabular format
+    """
+    raise NotImplementedError
+
+def display_year_report() -> None:
+    """
+    Display a detailed report on the year:
+        - summary: income, expenditure, net change
+        - table:   monthly income/expenditure/net change, categories
+        - charts:  select categories
+    """
+    raise NotImplementedError
+
+def display_category_records() -> None:
+    """
+    Display income/expenditure for a given category
+    """
+    raise NotImplementedError
+
+def get_investment_summary() -> None:
+    """
+    Display all current holdings, average buy price, current price, current value, profit, and profit percentage
+    """
+    raise NotImplementedError
+
+
+"""
+Editing the Database
+"""
+
+def insert_record() -> bool:
+    """
+    Insert a record of income/expenditure to the database.
+    Returns True if successful, False otherwise.
+    """
+    raise NotImplementedError
+
+def delete_record() -> bool:
+    """
+    Delete a record of income/expenditure from the database.
+    Returns True if successful, False otherwise.
+    """
+    raise NotImplementedError
+
+def edit_record() -> bool:
+    """
+    Edit a record of income/expenditure in the database.
+    Returns True if successful, False otherwise.
+    """
+    raise NotImplementedError
+
+def create_category() -> bool:
+    """
+    Create a new category.
+    Returns True if successful, False otherwise.
+    """
+    raise NotImplementedError
+
+def edit_category() -> bool:
+    """
+    Edit the name of a category.
+    Returns True if successful, False otherwise.
+    """
+    raise NotImplementedError
+
+def delete_category() -> bool:
+    """
+    Delete a category.
+    The category of records with this category will be changed to '-/-'.
+
+    Returns True if successful, False otherwise.
+    """
+    raise NotImplementedError
+
+"""
+Main
+"""
 
 if __name__ == "__main__":
-
     base_dir = os.path.dirname(__file__)
     conn, cur = initialise_db(base_dir)
