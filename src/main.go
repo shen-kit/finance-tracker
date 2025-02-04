e217a1f610160a2a7ba40d281db022c483f08255
Shen-Kit Hia <shenkit108+1@gmail.com>
2025-02-04 12:48:02 +1100

    creates function to test query methods that return a slice of structs

--- src/main.go
+++ src/main.go
@@ -9,38 +9,20 @@ import (
 func main() {
 	backend.SetupDb("./test.db")
 	// backend.CreateDummyData()
-	res, err := backend.GetInvestmentsRecent(0)
-	if err != nil {
-		println("error: ", err.Error())
-	}
-	fmt.Printf("result length: %d\n", len(res))
-	for _, r := range res {
-		fmt.Printf("%+v\n", r)
-	}
-	res, err = backend.GetInvestmentsFilter(backend.NewFilterOpts().WithCode("VG"))
-	if err != nil {
-		println("error: ", err.Error())
-	}
-	fmt.Printf("result length: %d\n", len(res))
-	for _, r := range res {
-		fmt.Printf("%+v\n", r)
-	}
 
-	var recRes []backend.Record
-	recRes, err = backend.GetRecordsRecent(0)
+	testQueryFunc(backend.GetInvestmentsRecent(0))
+	testQueryFunc(backend.GetInvestmentsFilter(backend.NewFilterOpts().WithCode("IV").WithMinCost(4000)))
+	testQueryFunc(backend.GetRecordsRecent(0))
+	testQueryFunc(backend.GetRecordsFilter(backend.NewFilterOpts().WithMinCost(0)))
+}
+
+func testQueryFunc[T any](res []T, err error) {
+	fmt.Printf("\n")
 	if err != nil {
 		println("error: ", err.Error())
 	}
-	fmt.Printf("result length: %d\n", len(recRes))
-	for _, r := range recRes {
-		fmt.Printf("%+v\n", r)
-	}
-	recRes, err = backend.GetRecordsFilter(backend.NewFilterOpts().WithCatId([]int{1, 2}))
-	if err != nil {
-		println("error: ", err.Error())
-	}
-	fmt.Printf("result length: %d\n", len(recRes))
-	for _, r := range recRes {
+	fmt.Printf("result length: %d\n", len(res))
+	for _, r := range res {
 		fmt.Printf("%+v\n", r)
 	}
 }
