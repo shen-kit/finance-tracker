951cc4bfcf1e44f848d5d98163250368adee68e6
Shen-Kit Hia <shenkit108+1@gmail.com>
2025-02-19 11:42:53 +1100

    sets up cmdline args for db path

--- src/main.go
+++ src/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	// "fmt"
+	// "os"
+
 	"github.com/shen-kit/finance-tracker/backend"
 	"github.com/shen-kit/finance-tracker/frontend"
 )
 
 func main() {
+	// if len(os.Args) != 2 {
+	// 	fmt.Println("Run using `$finance-tracker <path_to_db>`")
+	// 	os.Exit(0)
+	// }
+	// backend.SetupDb(os.Args[1])
+
 	backend.SetupDb("./test.db")
 	// backend.CreateDummyData()
 
