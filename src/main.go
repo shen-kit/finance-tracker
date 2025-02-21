4246e4c24b6c4daa1c2865d51be1bef4c1c8aff4
Shen-Kit Hia <shenkit108+1@gmail.com>
2025-02-21 12:58:16 +1100

    sets up main to take db path as argument

--- src/main.go
+++ src/main.go
@@ -1,22 +1,19 @@
 package main
 
 import (
-	// "fmt"
-	// "os"
+	"fmt"
+	"os"
 
 	"github.com/shen-kit/finance-tracker/backend"
 	"github.com/shen-kit/finance-tracker/frontend"
 )
 
 func main() {
-	// if len(os.Args) != 2 {
-	// 	fmt.Println("Run using `$finance-tracker <path_to_db>`")
-	// 	os.Exit(0)
-	// }
-	// backend.SetupDb(os.Args[1])
+	if len(os.Args) != 2 {
+		fmt.Println("Run using `$finance-tracker <path_to_db>`")
+		os.Exit(0)
+	}
 
-	backend.SetupDb("./test.db")
-	// backend.CreateDummyData()
-
+	backend.SetupDb(os.Args[1])
 	frontend.CreateTUI()
 }
